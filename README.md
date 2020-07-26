b3e431d9b48d34054cba4ce263903aa4d6326d8f
Aral Roca Gomez <aral-rg@hotmail.com>
2020-07-26 13:22:51 +0200

    Improvements (#1)
    * Sort by pub_date
    
    * Slice vect to display last 5 articles
    
    * Remove warnings
    
    * Refactor code into a 'create_readme' module

--- README.md
+++ README.md
@@ -16,18 +16,10 @@
 </p>
 
 
-#### 📖 Latest articles I wrote on [aralroca.com](https://aralroca.com):
+#### 📖 Latest 5 articles I wrote on [aralroca.com](https://aralroca.com):
  
-* [App with React API without tools as Webpack or Babel](https://aralroca.com/blog/app-with-react-api-without-tools-as-webpack-or-babel) 
-* [Image classifier: in the browser](https://aralroca.com/blog/cat-dog-classifier) 
-* [Detect text toxicity using React](https://aralroca.com/blog/detect-text-toxicity-with-react) 
-* [Discovering Snowpack](https://aralroca.com/blog/discovering-snowpack) 
-* [Don’t control everything! React forms](https://aralroca.com/blog/dont-control-everything-react-forms) 
-* [Exploring HTMLPortalElement with React](https://aralroca.com/blog/exploring-htmlportalelement-with-react) 
 * [First steps in WebGL](https://aralroca.com/blog/first-steps-in-webgl) 
-* [First steps with TensorFlow.js](https://aralroca.com/blog/first-steps-with-tensorflowjs) 
+* [Image classifier: in the browser](https://aralroca.com/blog/cat-dog-classifier) 
+* [Discovering Snowpack](https://aralroca.com/blog/discovering-snowpack) 
 * [From Node to Deno](https://aralroca.com/blog/from-node-to-deno) 
-* [Learn Deno: Chat app](https://aralroca.com/blog/learn-deno-chat-app) 
-* [Next-translate released 🎉](https://aralroca.com/blog/next-translate-released) 
-* [OpenCV directly in the browser (webassembly + webworker)](https://aralroca.com/blog/opencv-in-the-web) 
 * [How to create pagination badges](https://aralroca.com/blog/pagination-badges)
--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,10 @@
-extern crate rss;
+mod create_readme;
 
-use rss::Channel;
-use std::fs;
-
-struct FeedItem {
-    title: String,
-    link: String,
-    desc: String,
-    pub_date: String,
-}
-
-fn print_item(item: &FeedItem) {
-    println!("{}", item.title);
-    println!("{}", item.link);
-    println!("");
-}
-
-fn get_blog_rss() -> Vec<FeedItem> {
-    let items = Channel::from_url("https://aralroca.com/rss.xml")
-        .unwrap()
-        .items()
-        .iter()
-        .map(|item| FeedItem {
-            title: item.title().unwrap().to_string(),
-            link: item.link().unwrap().to_string(),
-            desc: item.description().unwrap().to_string(),
-            pub_date: item.pub_date().unwrap().to_string(),
-        })
-        .collect();
-
-    items
-}
-
-fn get_latest_articles() -> String {
-    let posts: Vec<FeedItem> = get_blog_rss();
-
-    return posts.iter().fold("".to_string(), |acc, item| {
-        format!("{} \n* [{}]({})", acc, item.title, item.link)
-    });
-}
-
-fn create_readme() {
-    let tpl =
-        fs::read_to_string("README.tpl").expect("Something went wrong reading the README.tpl file");
-    let last_articles = get_latest_articles();
-
-    fs::write(
-        "README.md",
-        tpl.replace("%{{latest_articles}}%", &last_articles),
-    );
-}
+use create_readme::create_readme;
 
 fn main() {
-    create_readme();
+    match create_readme() {
+        Ok(_v) => println!("README.md file generated correctly"),
+        Err(e) => println!("Opps! there was an error: {:?}", e),
+    }
 }
